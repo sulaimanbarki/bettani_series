d77107bd00708bcfd86b7246625bbdcb951e9956
Sulaiman Barki <99899527+sulaimanbarki@users.noreply.github.com>
2025-06-17 08:15:19 +0500

    Update test.blade.php

--- resources/views/front/pages/test.blade.php
+++ resources/views/front/pages/test.blade.php
@@ -22,7 +22,7 @@
 
                 @if (session('success'))
                     <div class="alert alert-success">
-                        {{ session('success') }}
+                        {{ session('success') }}  
                     </div>
                 @endif
                 <h4>Upcomming Tests</h4>
